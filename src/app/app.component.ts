4498fe1fd498923c307ff63d6c61ebf208454be8
Tibor Pilz <tibor@pilz.berlin>
2019-05-18 07:26:21 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import Tone from 'tone';
+
+import { SynthNode, HarmonizeNode, AddIntervalsNode, KeyboardNode, KeyboardOutputNode } from './graphNodes';
+
+const simpleSynth = new SynthNode();
+const major = new HarmonizeNode();
+const minor = new HarmonizeNode([0, 3, 7]);
+const addHigher = new AddIntervalsNode([12]);
+const keyboard = new KeyboardNode();
+const keyboardOutput = new KeyboardOutputNode();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'virtuaw-web';
+  triggerSynth = keyboard.trigger;
+  connect() {
+    keyboard.connect(minor).connect(addHigher).connect(keyboardOutput);
+  }
+  disconnect() {
+    simpleSynth.disconnect();
+    major.disconnect();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+import { KeyComponent } from './keyboard/key/key.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyboardComponent,
+    KeyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
